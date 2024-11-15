74b9743ff19bd02eff3cdad8cb573c8726b59f82
{doshishaus} <cgeh0721@mail3.doshisha.ac.jp>
2024-11-15 21:57:48 +0900

    カードコンポーネント

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,16 @@ import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter }
 import { Label, Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@radix-ui/react-select";
 import Link from "next/link";
 import { Input } from "postcss";
+import BBSCard from "./components/layout/BBSCard";
 
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Card className="w-[350px]">
-          <CardHeader>
-            <CardTitle>カードタイトル</CardTitle>
-            <CardDescription>カードの説明</CardDescription>
-          </CardHeader>
-          <CardContent>
-            カード内の内容
-          </CardContent>
-          <CardFooter className="flex justify-between">
-            <Link href={"/bbs-posts/1"}>詳細ページリンク</Link>
-          </CardFooter>
-        </Card>
+      <main className="flex gap-8 row-start-2 items-center sm:items-start">
+        <BBSCard/>
+        <BBSCard/>
       </main>
-
     </div>
   );
 }
